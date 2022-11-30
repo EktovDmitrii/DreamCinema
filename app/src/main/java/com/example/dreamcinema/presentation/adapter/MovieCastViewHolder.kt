cf089046cbabd428db7c674d2eaf97a6646be307
Dmitrii <actovus@gmail.com>
2022-11-30 17:46:38 +0300

    cast works fine

--- app/src/main/java/com/example/dreamcinema/presentation/adapter/MovieCastViewHolder.kt
+++ app/src/main/java/com/example/dreamcinema/presentation/adapter/MovieCastViewHolder.kt
@@ -2,22 +2,28 @@ package com.example.dreamcinema.presentation.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dreamcinema.R
 import com.example.dreamcinema.databinding.CastCardBinding
 import com.example.dreamcinema.domain.MovieCast
 import com.squareup.picasso.Picasso
 
-class MovieCastViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+class MovieCastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val binding: CastCardBinding = CastCardBinding.bind(itemView)
 
-    private val adapter = MovieCastAdapter()
 
-    fun bind(movieCast: MovieCast){
+    fun bind(movieCast: MovieCast) {
         binding.tvActorName.text = movieCast.name
         binding.tvActorInfo.text = movieCast.character
-        Picasso.get().load(BASE_URL + movieCast.profilePath)
-            .into(binding.ivActorImage)
+        if (movieCast.profilePath != null) {
+            Picasso.get().load(BASE_URL + movieCast.profilePath)
+                .into(binding.ivActorImage)
+        } else {
+            Picasso.get().load(R.drawable.ic_launcher_background)
+                .into(binding.ivActorImage)
+        }
     }
+
 
     companion object {
         private const val BASE_URL = "https://image.tmdb.org/t/p/original/"
